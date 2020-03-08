c3b210dcd29a085125808d4f9fa6acc2637c31ac
= <=>
2020-03-08 16:19:02 +0700

    chitietsp

---
+++ footer.php
@@ -0,0 +1,98 @@
+<div class="footer">
+         <div class="main-content">
+            <div class="cs-flex top">
+               <div class="cs-flex cs-col-12 cs-col-sm-12 cs-col-md-12 cs-col-lg-6 box-left">
+                  <div class="cs-col-12 cs-col-sm-12 cs-col-md-4 cs-col-lg-4 item">
+                     <div class="about">
+                        <h4 class="title">Giới thiệu chung</h4>
+                        <a class="link" href="front/tai-sao-ban-nen-mua-hang-tai-CODO SUPP-vn-316">Giới thiệu về CODO SUPP</a> <a class="link" href="front/doi-va-tra-hang-370">Chính sách đổi trả hàng</a> <a class="link" href="front/quy-dinh-chung-khi-mua-hang-tai-CODO SUPP">Quy định chung</a> <a class="link" href="front/lien-he">Liên hệ & khiếu nại</a> 
+                     </div>
+                  </div>
+                  <div class="cs-col-12 cs-col-sm-12 cs-col-md-4 cs-col-lg-4 item">
+                     <div class="about">
+                        <h4 class="title">Hỗ trợ khách hàng</h4>
+                        <a class="link" href="front/huong-dan-mua-hang-tai-CODO SUPP-vn-455">Hướng dẫn đặt hàng</a> <a class="link" href="front/phuong-thuc-thanh-toan-371">Hướng dẫn thanh toán</a> <a class="link" href="front/phuong-thuc-van-chuyen-372">Chính sách vận chuyển</a> <a class="link" href="front/chinh-sach-bao-mat-cua-CODO SUPP">Chính sách bảo mật</a> <a class="link" href="front/chinh-sach-bao-hanh-tai-CODO SUPP">Chính sách bảo hành</a> 
+                     </div>
+                  </div>
+                  <div class="cs-col-12 cs-col-sm-12 cs-col-md-4 cs-col-lg-4 item">
+                     <div class="about">
+                        <h4 class="title">Thông tin cần biết</h4>
+                        <a class="link" href="front/kiem-tra-doi-diem-tai-CODO SUPP-cuc-ky-don-gian">Kiểm tra tích điểm</a> <a class="link" href="front/san-deal">Săn hàng giá rẻ</a> <a class="link" href="front/c22/cach-tang-can-tang-co">Kiến thức tập Gym</a> <a class="link" href="front/c21/dinh-duong-the-hinh">Chế độ dinh dưỡng</a> 
+                     </div>
+                  </div>
+               </div>
+               <div class="cs-flex cs-col-12 cs-col-sm-12 cs-col-md-12 cs-col-lg-6 box-right">
+                  <div class="cs-col-12 cs-col-sm-12 cs-col-md-6 cs-col-lg-6 item">
+                     <div class="about">
+                        <h4 class="title">Về chúng tôi</h4>
+                        <div class="cs-flex phone">
+                           <div class="image"> </div>
+                           <div class="box">
+                              <div class="des">Tư vấn & đặt hàng:</div>
+                              <a class="number" href="front/tel:123.456.789" rel="nofollow">123.456.789</a> 
+                           </div>
+                        </div>
+                        <a class="link" href="front/javascript:;">Phục vụ tất cả các ngày trong tuần</a> <a class="link" href="front/#">Bắt đầu mở cửa bán hàng từ 8h30</a> 
+                     </div>
+                     <div><a href="front/danh-sach-ngan-hang-quy-khach-co-the-chuyen-tien-khi-mua-hang-tai-CODO SUPP-685" style="font-weight: 550; background: #dd9e44; color: #FFF;padding: 5px;margin-top: 8px;display: inline-block;">Danh sách tài khoản ngân hàng</a></div>
+                     <div class="cs-col-12 cs-col-sm-12 cs-col-md-6 cs-col-lg-6 item">
+                        <p style="color: #acacac; margin-bottom: 8px;">Chấp nhận thanh toán</p>
+                        <img class="lazy" data-src="front/upload/images/payment.png" alt="Chấp nhận thanh toán qua thẻ ngân hàng" title="Chấp nhận thanh toán qua thẻ ngân hàng" /> 
+                     </div>
+                  </div>
+                  <div class="cs-col-12 cs-col-sm-12 cs-col-md-6 cs-col-lg-6 item">
+                     <div class="about">
+                        <h4 class="title">Facebook page</h4>
+                        <div class="cs-flex">
+                           <div class="fb-page" style="width:100%" data-href="front/http://facebook.com/" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
+                              <blockquote cite="http://facebook.com/" class="fb-xfbml-parse-ignore"> <a href="front/http://facebook.com/" rel="nofollow"> - Thực phẩm thể hình</a> </blockquote>
+                           </div>
+                        </div>
+                     </div>
+                     <p><img class="lazy" data-src="front/upload/images/dathongbao.png" alt="đã thông báo với bộ công thương" title="đã thông báo với bộ công thương" width="130" /></p>
+                  </div>
+               </div>
+            </div>
+            <div class="center">
+               <h3 class="title" align="center"> Hệ Thống cửa hàng bán lẻ</h3>
+               <div class="cs-flex">
+                  <div class="connect cs-col-12 cs-col-sm-12 cs-col-md-4 cs-col-lg-4">
+                     <h4 class="topic">Lorem ipsum dolor sit  </h4>
+                     <span class="lable">Lorem ipsum dolor sit amet consectetur adipisicing elit</span> <span class="lable">Giờ mở cửa: 8h30 tới 23h đêm</span> <span class="lable">Hotline: <a href="#" rel="nofollow" class="hover-color">123.123.123</a></span> 
+                  </div>
+                  <div class="connect cs-col-12 cs-col-sm-12 cs-col-md-4 cs-col-lg-4">
+                     <h4 class="topic">Lorem ipsum dolor sit</h4>
+                     <span class="lable">Lorem ipsum dolor sit amet consectetur adipisicing elit</span> <span class="lable">Giờ mở cửa: 8h30 tới 21h tối</span> <span class="lable">Hotline: <a href="#" rel="nofollow" class="hover-color">123.123.123</a></span> 
+                  </div>
+                  <div class="connect cs-col-12 cs-col-sm-12 cs-col-md-4 cs-col-lg-4">
+                     <h4 class="topic">Lorem ipsum dolor sit</h4>
+                     <span class="lable">Lorem ipsum dolor sit amet consectetur adipisicing elit</span> <span class="lable">Giờ mở cửa: 8h30 tới 21h tối</span> <span class="lable">Hotline: <a href="front/tel:123.123.123" rel="nofollow" class="hover-color">123.123.123</a></span> 
+                  </div> 
+               </div>
+               <div class="cs-flex" id="tags_hot_bottom"> <a href="front/tim-kiem3f0a?tukhoa=muscle+mass">Muscle Mass</a> / <a href="front/tim-kiem7042?tukhoa=super+mass">Super Mass</a> / <a href="front/tim-kiemd44f?tukhoa=serious+mass">Serious Mass</a> / <a href="front/tim-kiem69fe?tukhoa=mutant+mass">Mutant Mass</a> / <a href="front/tim-kiemee17?tukhoa=mass+fusion">Mass Fusion</a> / <a href="front/tim-kiemfcf0?tukhoa=mass+effect">Mass Effect</a> / <a href="front/tim-kiemb26c?tukhoa=whey+gold">Whey Gold</a> / <a href="front/tim-kiem2014?tukhoa=rule1">Rule 1</a> / <a href="front/tim-kiemc1d7?tukhoa=iso+100">Iso 100</a> / <a href="front/tim-kieme3bd?tukhoa=iso+surge">Iso Surge</a> / <a href="front/tim-kiem6a4b?tukhoa=vp2">Vp2</a> / <a href="front/tim-kiem70fc?tukhoa=best+bcaa">Best BCAA</a> / <a href="front/tim-kiem16e8?tukhoa=creatine">Creatine</a> / <a href="front/tim-kiemd6d8?tukhoa=iso+hd">Iso HD</a> / <a href="front/tim-kiemc379?tukhoa=omega3">Omega 3</a> / <a href="front/tim-kiemafa1?tukhoa=c4">C4</a> / <a href="front/tim-kiem5027?tukhoa=mr+hyde">Mr.Hyde</a> / <a href="front/tim-kiemc38a?tukhoa=the+curse">The Curse</a> / <a href="front/tim-kiem0b00?tukhoa=the+ripper">The Ripper</a> / <a href="front/tim-kiem3675?tukhoa=impact+isolate">Impact Isolate</a> / <a href="front/tim-kiem65da?tukhoa=impact+protein">Impact Protein</a> / <a href="front/tim-kiem5815?tukhoa=semtex">Semtex</a> / <a href="front/tim-kiemd1bc?tukhoa=hydropure">Hydropure</a> / <a href="front/tim-kiembae2?tukhoa=mass+tech">Mass Tech</a> / <a href="front/tim-kieme231?tukhoa=vitabolic">Vitabolic</a> / <a href="front/tim-kiem5f6e?tukhoa=anabolic+state">Anabolic State</a> / <a href="front/tim-kiemf022?tukhoa=aggro">Aggro</a> / <a href="front/tim-kiemc2d4?tukhoa=fish+oil">Fish Oil</a> / <a href="front/tim-kiem2754?tukhoa=nitrotech">Nitrotech</a> / <a href="front/tim-kiemc5aa?tukhoa=casein">Casein</a> / <a href="front/tim-kiem4418?tukhoa=iso+flex">Iso Flex</a> / <a href="front/tim-kiemaadc?tukhoa=pro+gainer">Pro Gainer</a>. </div>
+            </div>
+         </div>
+         <div class="bottom">
+            <div class="main-content cs-flex">
+               <div class="mr-auto panel"> <span>Copy right ©2020</span> </div>
+               <div class="panel"> <a href="front/#" class="hover-color-yellow">Trang chủ</a> <a href="#" class="hover-color-yellow">Kiến thức Gym</a> <a href="front/lien-he" class="hover-color-yellow">Phản ánh, khiếu nại</a> </div>
+            </div>
+         </div>
+         <div class="phone_bottom" style="bottom: 10px;"><a href="front/tel:0919013030"><span class="active_phone"><i class="fa fa-phone"></i></span></a></div>
+      </div>
+      <div style="padding: 10px;">
+         <div style="text-align: center; font-size: 14px;">Tất cả các sản phẩm mà CODO SUPP bán không phải là thuốc, không thể thay thế thuốc chữa bệnh.<br />Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni obcaecati cupiditate, blanditiis temporibus nobis unde asperiores nostrum ea reiciendis, ut molestiae ex non accusamus id odio, doloribus earum consequatur nihil!.</div>
+      </div>
+      <div id="fb-root"></div>
+     
+      <link rel="stylesheet" href="front/asset/site/modal/jquery.modal.min.css" />
+      <script src="front/asset/site/js/jquery.lazy.min.js"></script> 
+      <script> $(function() { $('.lazy').Lazy();});</script>  
+      <script src="front/asset/site/js/action1.js"></script> 
+      <script src="front/asset/site/js/main1.js"></script> 
+      <script src="front/asset/site/js/validation1.js"></script> 
+      <script src="front/asset/site/magic/jquery.magic.js"></script> 
+      <script src="front/asset/site/modal/jquery.modal.min.js"></script>   
+
+   </body> 
+</html>
--- header.php
+++ header.php
@@ -5,7 +5,7 @@
    <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
    <!-- /Added by HTTrack -->
    <head>
-      <link rel="shortcut icon" href="front/upload/logo/icon_logo.png"/>
+      <link rel="shortcut icon" href="front/upload/logo/logo.jpg"/>
       <link rel="stylesheet" href="front/asset/site/js/slider/bxslider.css">
       <link rel="stylesheet" href="front/asset/site/css/fontawesome/css/all.css">
       <link rel="stylesheet" href="front/asset/site/css/bootstrap.min.css">
@@ -48,3 +48,151 @@
          gtag('config', 'UA-105598555-1');
       </script> 
    </head>
+   <body>
+      <!-- Google Tag Manager (noscript) --> 
+      <noscript><iframe src="https://www.googletagmanager.com/ns?id=GTM-PZKMVKR" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+      <!-- End Google Tag Manager (noscript) --> <!-- Top Banner --> 
+      <div class="slider-ads-top banner" numColumn='[{"num": 1, "width": 0}]' classItems="item" isControls="false" isPager="false"> </div>
+      <script>$('.slider-ads-top').bxSlider({auto: true,pager: false,autoHover: true,mode: 'horizontal',controls: false,pause: 10000});</script> <!-- End Top Banner --> 
+      <p style="text-align: center; padding: 0 10px; margin: 15px 0; line-height: 21px;"><a style="font-size:16px" href="front/tdee_calculator" target="_blank">Ghé thăm <span style="background: #00aeef; color: #FFF; padding: 2px 5px 3px 5px; border-radius: 5px;">ứng dụng tính TDEE</span> BÁ ĐẠO NHẤT VIỆT NAM - đo được lượng calories và tính được thực đơn mỗi ngày của bạn để tăng cân, tăng cơ hoặc giảm mỡ theo ý muốn tại đây</a></p>
+      <div class="header">
+         <!-- Top bar --> 
+         <div class="top-bar hidden-top-bar">
+            <div class="main-content cs-flex">
+               <div class="cs-col-12 cs-col-sm-6 cs-flex"> 
+                <a href="front/tel:123.456.789" rel="nofollow" class="connect cs-flex flex-row flex-align-center mr-20">
+                 <span><i class="fas fa-phone-square"></i> Hotline: 123.456.789</span>
+                </a>
+                <a href="#" target="_blank" rel="nofollow" class="connect cs-flex flex-row flex-align-center mr-20">
+                   <span><i class="fab fa-facebook-square"></i> Ghé Fanpage</span>
+                </a> 
+                <a href="#" rel="nofollow" class="checkpoint cs-flex flex-row flex-align-center mr-20"> 
+                    <span style="color: #FFF"><i class="fas fa-address-card" style="color: #FFF"></i> Kiểm tra tích điểm</span> 
+                </a> 
+            </div>
+               <div class="cs-col-12 cs-col-sm-6 cs-flex tool-buttons">  
+                <a href="#" rel="modal:open" class="connect cs-flex flex-row flex-align-center ml-auto"> Đăng nhập </a> 
+                <a href="#" class="connect cs-flex flex-row flex-align-center"> Tài khoản của tôi </a> 
+            </div>
+            </div>
+         </div>
+         <!-- End top bar --> <!--Top Bar User Mobile--> 
+         <div id="topUserMobile"><a href="#" rel="nofollow"><span style="color: #FFF"><i class="fas fa-address-card" style="color: #FFF"></i> Kiểm tra tích điểm</span></a> <a href="#"><i class="fas fa-user-shield" style="color: #FFF"></i> Tài khoản của tôi</a> </div>
+         <!--End Top Bar User Mobile--> <!--Top Bar Mobile--> 
+         <div id="topMobile" style="background: #00aeef; box-shadow: 0 2px 10px #eae69a; padding: 10px 20px 5px 10px;">
+            <a class="switch-menu" style="cursor: pointer; float: left; width: 23%; margin-top: 8px;" href="front/#"><i class="fas fa-bars" style="color: #FFF;"> MENU</i></a> <a style="float: left; width: 20%" href="front/index" class="switch-menu mb-10 show-top-bar box-icon cs-flex flex-column flex-align-center flex-content-center mr-auto"><img src="front/upload/logo/logo.jpg" alt="CODO SUPP - Dinh dưỡng, thực phẩm thể hình, Gym chính hãng." style="width: 80px;"></a> <a style="float: right; width: 53%; margin-top: 8px; color: #FFF; font-weight: bold;" href="front/home/store" rel="modal:open" class="mb-10 show-top-bar box-icon cs-flex flex-column flex-align-center flex-content-center ml-auto" style="color: #FFF; font-weight: bold;">Bấm xem 6 chi nhánh</a> 
+            <div style="clear: both;"></div>
+         </div>
+         <!--End Top Bar Mobile--> <!-- Content --> 
+         <div class="content">
+            <div class="cs-flex main-content">
+               <div class="cs-col-12 cs-col-lg-3 flex-align-center flex-content-center cs-flex">
+                  <!-- Box icon --> <a href="#" class="switch-menu mb-10 show-top-bar box-icon cs-flex flex-column flex-align-center flex-content-center mr-auto"> <i class="fas fa-bars cm-red"> DANH MỤC</i> </a> <!-- End box icon --> 
+                  <div class="logo"> <a href="#"><img src="front/upload/logo/logo.jpg" alt="CODO SUPP - Dinh dưỡng, thực phẩm thể hình, Gym chính hãng."></a> </div>
+                  <!-- Box icon --> 
+                  <a href="#" class="mb-10 show-top-bar box-icon cs-flex flex-column flex-align-center flex-content-center ml-auto cart">
+                     <i class="fas fa-shopping-cart"></i> 
+                     <div class="box-content"> Xem giỏ hàng </div>
+                  </a>
+                  <!-- End box icon --> 
+               </div>
+               <div class="cs-col-12 cs-col-lg-6 cs-flex flex-column ">
+                  <!-- Form search --> 
+                  <form action=" front/tim-kiem" class="form-search cs-flex flex" method="get"> <input name="tukhoa" type="text" class="flex" style="font-size: 15px;" placeholder="Tìm sản phẩm và Enter..." value=""> <button> <i class="fa fa-search"></i> </button> </form>
+                  <!-- End form search --> <!-- Sugges --> 
+                  <div class="cs-flex sugges hidden-top-bar">
+                     <div class="label mr-10">Gợi ý: </div>
+                     <div style="white-space: nowrap;overflow:hidden;width:calc(100% - 50px)"> <a href="front/tim-kiem9172?tukhoa=Bcaa" class=" mr-5">Bcaa,</a> <a href="front/tim-kiemd0fd?tukhoa=Mass" class=" mr-5">Mass,</a> <a href="front/tim-kiem9b33?tukhoa=whey" class=" mr-5">whey,</a> <a href="front/tim-kiem792d?tukhoa=Iso%20100" class=" mr-5">Iso 100,</a> <a href="front/tim-kiem2ed3?tukhoa=rule%201" class=" mr-5">rule 1,</a> .v.v. </div>
+                  </div>
+                  <!-- End sugges --> 
+               </div>
+               <div class="cs-col-3 cs-flex">
+                  <!-- Box icon --> 
+                  <a href="#" class="hidden-top-bar box-icon cs-flex flex-column flex-align-center flex-content-center ml-auto">
+                     <i class="far fa-heart"></i> 
+                     <div class="box-content"> Yêu thích </div>
+                  </a>
+                  <!-- End box icon --> <!-- Box icon --> 
+                  <a href="#" class="hidden-top-bar box-icon cs-flex flex-column flex-align-center flex-content-center ml-20 cart">
+                     <i class="fas fa-shopping-cart"></i> 
+                     <div class="box-content"> Xem giỏ hàng </div>
+                  </a>
+                  <!-- End box icon --> 
+               </div>
+            </div>
+         </div>
+         <!-- End content --> <!-- Menu --> 
+         <div class="menu">
+            <div class="main-content">
+               <ul class="list-menu">
+                  <li><a class="cs-flex flex-align-center" href="#" style="background: #f35f64;">Trang chủ</a></li>
+                  <li>
+                     <a href="#" class="cs-flex flex-align-center">Sữa tăng cân</a>
+                     <ul>
+                        <li> <a href="#" class="cs-flex flex-align-center">Tăng cân nhanh</a></li>
+                        <li> <a href="#" class="cs-flex flex-align-center">Tăng cân nạc, ít mỡ</a></li>
+                     </ul>
+                  </li>
+                  <li>
+                     <a href="front/c152/whey-protein-tang-co" class="cs-flex flex-align-center">Whey Protein</a>
+                     <ul>
+                        <li> <a href="#" class="cs-flex flex-align-center">Whey Protein Blend</a></li>
+                        <li> <a href="#" class="cs-flex flex-align-center">Whey Protein Isolate</a></li>
+                        <li> <a href="#" class="cs-flex flex-align-center">Hydrolyzed Whey</a></li>
+                        <li> <a href="#" class="cs-flex flex-align-center">Casein Protein</a></li>
+                        <li> <a href="#" class="cs-flex flex-align-center">Meal Replacement</a></li>
+                     </ul>
+                  </li>
+                  <li>
+                     <a href="#" class="cs-flex flex-align-center">BCAA</a>
+                     <ul></ul>
+                  </li>
+                  <li>
+                     <a href="#" class="cs-flex flex-align-center">Tăng sức mạnh</a>
+                     <ul>
+                        <li> <a href="#" class="cs-flex flex-align-center">Pre-workout</a></li>
+                        <li> <a href="#" class="cs-flex flex-align-center">Pre & giảm mỡ</a></li>
+                        <li> <a href="#" class="cs-flex flex-align-center">Creatine</a></li>
+                        <li> <a href="#" class="cs-flex flex-align-center">Đơn chất</a></li>
+                     </ul>
+                  </li>
+                  <li>
+                     <a href="front/c162/vitamin-khoang-chat" class="cs-flex flex-align-center">Vitamin, khoáng</a>
+                     <ul>
+                        <li> <a href="front/bo-sung-vitamin" class="cs-flex flex-align-center">Bổ sung Vitamin</a></li>
+                        <li> <a href="front/tang-hoc-mon-nam" class="cs-flex flex-align-center">Tăng hóc môn nam</a></li>
+                        <li> <a href="front/bo-sung-dau-ca" class="cs-flex flex-align-center">Bổ sung dầu cá</a></li>
+                     </ul>
+                  </li>
+                  <li>
+                     <a href="front/c158/giam-can-dot-mo" class="cs-flex flex-align-center">Giảm mỡ</a>
+                     <ul>
+                        <li> <a href="front/sinh-nhiet-dot-mo" class="cs-flex flex-align-center">Sinh nhiệt đốt mỡ</a></li>
+                        <li> <a href="front/it-chat-kich-thich" class="cs-flex flex-align-center">Ít chất kích thích</a></li>
+                     </ul>
+                  </li>
+                  <li>
+                     <a href="front/c165/phu-kien" class="cs-flex flex-align-center">Phụ kiện</a>
+                     <ul>
+                        <li> <a href="front/c36/binh-lac" class="cs-flex flex-align-center">Bình lắc</a></li>
+                        <li> <a href="front/c39/phu-kien-khac" class="cs-flex flex-align-center">Phụ kiện khác</a></li>
+                     </ul>
+                  </li>
+                  <li>
+                     <a href="front/combo-gia-tot" class="cs-flex flex-align-center">COMBO X2</a>
+                     <ul>
+                        <li> <a href="front/combo-tang-can-tang-co" class="cs-flex flex-align-center">Combo tăng cân tăng cơ</a></li>
+                        <li> <a href="front/combo-tang-co-giam-mo" class="cs-flex flex-align-center">Combo tăng cơ giảm mỡ</a></li>
+                     </ul>
+                  </li>
+                  <li class="show-top-bar"> <a href="front/home/store" class="cs-flex flex-align-center">Hệ thống 6 cửa hàng</a> </li>
+                  <li class="show-top-bar"><a href="front/#" onclick="closeMenu();">Đóng lại (X)</a></li>
+               </ul>
+            </div>
+         </div>
+         <!-- End menu --> 
+        
+      </div>
+      </div>
+     <h1 style="display: none;">CODO SUPP - Dinh dưỡng, thực phẩm thể hình, Gym chính hãng.</h1>
+      <h2 style="display: none">CODO SUPP chỉ phân phối các sản phẩm thực phẩm bổ sung chính hãng</h2>
