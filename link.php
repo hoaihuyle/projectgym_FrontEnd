2d12b882ddcae328e3980325e94e3c7538ba7f26
= <=>
2020-03-09 23:30:51 +0700

    lienket

---
+++ link.php
@@ -0,0 +1,21 @@
+<?php include('header.php') ?>
+
+<section id="changelink">
+    <div class="container">
+        <div id="change">
+            <h1>Danh sách liên kết các trang</h1>
+            <ul>
+                <li><a href="index.php"> trang chủ</a></li>
+                <li><a href="bankinfo.php">thanh toán</a></li>
+                <li><a href="about_us.php">giới thiệu</a></li>
+                <li><a href="products.php">Danh sách sản phẩm</a></li>
+                <li><a href="product_detail.php">chi tiết sản phẩm</a></li>
+                <li><a href="signup.php"> đăng ký / đăng nhập</a></li>
+                <li><a href="card.php">giỏ hàng</a></li>
+                <li><a href="search.php">Tìm kiếm</a></li> 
+            </ul>
+        </div>
+    </div>
+</section>
+
+<?php include('footer.php') ?>
